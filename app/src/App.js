420526c67a693da0f1e2ff939a4bf166673b6d0d
Greg Wilson <gjwilson7390@gmail.com>
2021-03-10 12:51:38 -0600

    Basic header with nav links not yet functioning made

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={logo} id="logo"></img>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Our Cars</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
