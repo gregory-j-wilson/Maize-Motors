f50745d3de578b79a945730cf2394d340ef2fefa
Greg Wilson <gjwilson7390@gmail.com>
2021-03-12 01:06:28 -0600

    routes/pages added

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.png'
 import './App.css';
+import {Link, Route} from 'react-router-dom'
+import About from './Components/About'
+import OurCars from './Components/OurCars'
+import Contact from './Components/Contact'
 
 function App() {
   return (
@@ -8,13 +12,28 @@ function App() {
         <img src={logo} id="logo" alt="Maize Motors Logo"></img>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Our Cars</li>
-            <li>Contact</li>
+            <li><Link to="/" className="link">Home</Link></li>
+            <li><Link to="/about" className="link">About</Link></li>
+            <li><Link to="/cars" className="link">Our Cars</Link></li>
+            <li><Link to="/contact" className="link">Contact Us</Link></li>
           </ul>
         </nav>
       </header>
+
+      <Route exact path="/"></Route>
+      
+      <Route path="/about">
+        <About />
+      </Route>
+
+      <Route path="/cars">
+        <OurCars />
+      </Route>
+
+      <Route path="/contact">
+        <Contact />
+      </Route>
+
     </div>
   );
 }
---
+++ app/src/Components/OurCars.js
@@ -0,0 +1,8 @@
+export default function OurCars() {
+
+    return (
+        <div>
+            <h2>Our Cars</h2>
+        </div>
+    )
+}
