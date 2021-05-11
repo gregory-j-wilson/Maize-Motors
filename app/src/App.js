b461510caa43572dfb8ca861a2982b2ed08aa053
Greg Wilson <gjwilson7390@gmail.com>
2021-05-11 10:21:07 -0500

    homepage updated

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.png'
+import logo from './logo.jpg'
+import phone from './phone.jpg'
+import location from './location.jpg'
+import car from './car-red.jpg'
+
 import './App.css';
 import {Link, Route} from 'react-router-dom'
 import About from './Components/About'
 import OurCars from './Components/OurCars'
 import Contact from './Components/Contact'
+import Login from './Components/Login'
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <img src={logo} id="logo" alt="Maize Motors Logo"></img>
+        <div class="location-phone"><img width="70" src={location} alt="location-icon"></img>5203 N Maize Rd, Maize, KS 67101</div>
+        <div class="location-phone"><img width="50" src={phone} alt="phone-icon"></img><a href="tel:3163644196"><strong>(316) 364-4196</strong></a></div>
+        <img src={car} alt="red car" width="300"></img>
         <nav>
           <ul>
             <li><Link to="/" className="link">Home</Link></li>
@@ -32,6 +40,10 @@ function App() {
 
       <Route path="/contact">
         <Contact />
+      </Route>
+
+      <Route path="/admin">
+        <Login />
       </Route>
 
     </div>
