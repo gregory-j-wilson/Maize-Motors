8f6d163cf1605ec49217f33a4b9e1f9767e01e5d
Greg Wilson <gjwilson7390@gmail.com>
2021-05-11 10:25:20 -0500

    style changes on homepage

--- app/src/App.js
+++ app/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <img src={logo} id="logo" alt="Maize Motors Logo"></img>
+        <img style={{paddingBottom: 50 + 'px'}} src={logo} id="logo" alt="Maize Motors Logo"></img>
         <div class="location-phone"><img width="70" src={location} alt="location-icon"></img>5203 N Maize Rd, Maize, KS 67101</div>
         <div class="location-phone"><img width="50" src={phone} alt="phone-icon"></img><a href="tel:3163644196"><strong>(316) 364-4196</strong></a></div>
         <img src={car} alt="red car" width="300"></img>
